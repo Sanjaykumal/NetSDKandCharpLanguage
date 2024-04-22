bece106dc87472e63cbdd9ef52d19f56691cebb7
Sanjay <sk5248916@gmail.com>
2024-04-22 23:39:38 +0545

    deleted bin and obj file

--- LanguageBasics/NETStarter/Basics/DataAccess/Teacher.cs
+++ LanguageBasics/NETStarter/Basics/DataAccess/Teacher.cs
@@ -3,7 +3,7 @@ using System;
 public class Teacher
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string ame { get; set; }
     public string? Address { get; set; }
     public string? Qualification { get; set; }
     public char Gender { get; set; } = 'F';
