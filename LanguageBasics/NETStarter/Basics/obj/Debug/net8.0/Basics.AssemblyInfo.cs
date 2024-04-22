bed7ecafa2a903b8eeb67665dcda6e9898d6edfe
Sanjay <sk5248916@gmail.com>
2024-04-22 23:04:20 +0545

    upto College.db creation

--- LanguageBasics/NETStarter/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
+++ LanguageBasics/NETStarter/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d3c18a5f377763c9d47734a1ce1b9088f916b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
